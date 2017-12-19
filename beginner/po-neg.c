6d6f75e136d7a43c8f4f0283238d3b94f7268d4a
SRIYAMINIS <34641544+SRIYAMINIS@users.noreply.github.com>
2017-12-19 04:22:29 +0000

    Update po-neg.c

--- beginner/po-neg.c
+++ beginner/po-neg.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+   int a;
+   scanf("%d",&a);
+   if(a<0){
+   	printf("negative number");
+   }
+   else if(a>0){
+   	printf("positive number");
+   }
+   else{
+   	printf("zero");
+   }
+	return 0;
+}
 
