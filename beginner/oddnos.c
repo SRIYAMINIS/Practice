827328d9bb74ba8facdbded94bc5e20602dface9
SRIYAMINIS <34641544+SRIYAMINIS@users.noreply.github.com>
2018-03-06 16:28:02 +0530

    Create oddnos.c

---
+++ beginner/oddnos.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+int main(void)
+{
+	int a,b,i,ans=0;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	if(a<=0 && b<=0)
+	{
+		printf("%d",ans);
+	}
+	else
+	{
+		for(i=a+2;i<b;i+=2)
+		
+		{
+			if(i%2==1)
+			printf("%d ",i);
+		
+		}
+	}
+	return 0;
+}
+
+   
