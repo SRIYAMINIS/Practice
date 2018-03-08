f915c58b71215f8e9b06600d66771556b9ffd4a5
SRIYAMINIS <34641544+SRIYAMINIS@users.noreply.github.com>
2018-03-08 17:36:23 +0530

    Update factors of no.c

--- beginner/factors of no.c
+++ beginner/factors of no.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() 
 {
-	int n,i;
-	printf("enter the number:");
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
+  int n,i;
+  printf("enter the number:");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
 	{
-		if(n%i==0)
+	  if(n%i==0)
 		{
-			printf("%d ",i);
+		  printf("%d ",i);
 		}
 	}
 	return 0;
